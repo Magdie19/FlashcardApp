d0bb1eafa28d3fc9afbcecc2e4f4dd7e5a0d40cf
Magdie19 <mariemagdalasaintil19@gmail.com>
2025-09-20 11:39:23 -0400

    Ajout de Edit Text dans la nouvelle page

--- app/src/main/java/com/example/flashcardapp/MainActivity2.kt
+++ app/src/main/java/com/example/flashcardapp/MainActivity2.kt
@@ -1,7 +1,9 @@
 package com.example.flashcardapp
 
+import  android.content.Intent
 import android.os.Bundle
 import android.view.View
+import android.widget.ImageView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -12,15 +14,19 @@ class MainActivity2 : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main2)
+
+        val cancelButton = findViewById<ImageView>(R.id.cancel_button)
+        cancelButton.setOnClickListener{
+            val i = Intent(this@MainActivity2, MainActivity::class.java)
+            startActivity(i)
+            this.finish();
+        }
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
 
-        fun onSubmit(v: View) {
-            // closes the activity and returns to first screen
-            this.finish();
-        }
     }
 }
